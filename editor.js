d9fb62bc107e2945f183c4857d17712e7aabe2b4
YSAS4331 <ryo.matsu0619@gmail.com>
2026-03-20 11:25:55 +0900

    Update editor.js

--- editor.js
+++ editor.js
@@ -167,7 +167,7 @@ header { position: sticky; top: 0; z-index: 10; padding: .1rem 1rem; background:
     }
   
     try {
-      const { tokenize } = await import(`https://cdn.jsdelivr.net/gh/ysas4331/Editor@main/lexers/${l}.min.js`);
+      const { tokenize } = await import(`https://cdn.jsdelivr.net/gh/ysas4331/Editor@main/lexers/${l}.min.js?cache=${performance.now()}`);
   
       Editor.tokenizers[l] = tokenize;
   
